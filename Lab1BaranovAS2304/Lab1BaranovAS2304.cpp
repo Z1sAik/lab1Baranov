4a2fd6f37de92cde19774d9b151360c086ca6b01
Z1sAik <ilab807@gmail.com>
2024-09-05 16:59:56 +0300

    Лаб 1 продолжение. Добавленая отдельная структура для КС и бета версия основного меню.

--- Lab1BaranovAS2304/Lab1BaranovAS2304.cpp
+++ Lab1BaranovAS2304/Lab1BaranovAS2304.cpp
@@ -6,13 +6,26 @@ struct Pipe_settings
     std::string Name; //название трубы
     int length; //километры 
     int diameter; //миллиметры
+    bool repair; //признак "в ремонте" 
+};
+
+struct compressor_station 
+{
+    std::string Name; //название КС
+    int workshops; //Кол-во цехов
+    int workshopsinwork; //Кол-во цехов в работе
+    int effectiveness; //Коэфф. эффективности
 };
 
 int main(){
     setlocale(LC_ALL, "RU");
-    Pipe_settings Pipe;
-    std::cout << "Введите название трубы: ";
+    int x = 0;
+    do {
+        std::cout << "Меню"<<std::endl;
+    } while (x != 1);
+    /*Pipe_settings Pipe;
+    std::cout << "Введите название трубы (на английском язык): ";
     getline(std::cin, Pipe.Name);
-    std::cout << Pipe.Name;
+    std::cout << Pipe.Name;*/
     return 0;
 }
