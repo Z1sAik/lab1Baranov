e0eef255b21e5310ee1d596abe57c10d6c5ba602
Z1sAik <ilab807@gmail.com>
2024-09-05 16:37:18 +0300

    Лаб 1 начало. Создана структура Pipe_settings и добавлены нужные параметры. А также возможность задать своё имя трубы

---
+++ Lab1BaranovAS2304/Lab1BaranovAS2304.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+struct Pipe_settings
+{
+    std::string Name; //название трубы
+    int length; //километры 
+    int diameter; //миллиметры
+};
+
+int main(){
+    setlocale(LC_ALL, "RU");
+    Pipe_settings Pipe;
+    std::cout << "Введите название трубы: ";
+    getline(std::cin, Pipe.Name);
+    std::cout << Pipe.Name;
+    return 0;
+}
