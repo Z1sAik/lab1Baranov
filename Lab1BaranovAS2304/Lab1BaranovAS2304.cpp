6d4c3c3b3a92f2dab8f100e1a8dae250fae60a9c
Z1sAik <ilab807@gmail.com>
2024-09-07 13:56:21 +0300

    Лаб 1 Меню. Добавлены подпункты в меню для пункат 1) и 0)

--- Lab1BaranovAS2304/Lab1BaranovAS2304.cpp
+++ Lab1BaranovAS2304/Lab1BaranovAS2304.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <string>
-
+//Перменные
+int k = 0;
+//структуры
 struct Pipe_settings
 {
     std::string Name; //название трубы
@@ -16,10 +18,8 @@ struct compressor_station
     int workshopsinwork; //Кол-во цехов в работе
     int effectiveness; //Коэфф. эффективности
 };
-
-int main(){
-    setlocale(LC_ALL, "RU");
-    int x = -1;
+//Функции
+void menu() {
     std::cout << "Меню" << std::endl;
     std::cout << "1) Добавить трубу" << std::endl;
     std::cout << "2) Добавить КС" << std::endl;
@@ -30,12 +30,53 @@ int main(){
     std::cout << "7) Загрузить" << std::endl;
     std::cout << "0) Выход" << std::endl;
     std::cout << "Введите число: ";
-    do {
-        std::cin >> x;
-    } while (x == 0, std::cout << x);
-    /*Pipe_settings Pipe;
+    (std::cin >> k).get();
+}
+void menu_new_pipe() {
+    Pipe_settings Pipe;
     std::cout << "Введите название трубы (на английском язык): ";
     getline(std::cin, Pipe.Name);
-    std::cout << Pipe.Name;*/
+    std::cout << "Введите длину трубы в километрах: ";
+    (std::cin >> Pipe.length).get();
+    std::cout << "Введите диаметр трубы в миллиметрах: ";
+    (std::cin >> Pipe.diameter).get();
+    std::cout << "Выберите в каком состоянии труба: " << std::endl;
+    std::cout << "1)В ремонте" << std::endl;
+    std::cout << "2)Не в ремонте" << std::endl;
+    int n = 0;
+    (std::cin >> n).get();
+    while (n != 1 && n != 2) {
+        std::cout << "Выберите число 1 или 2: ";
+        (std::cin >> n).get();
+    };
+    if (n == 1) {
+        Pipe.repair = true;
+    }
+    else if (n == 2)
+    {
+        Pipe.repair = false;
+    }
+    if (Pipe.repair == true) {
+        std::cout << "Труба создана: " << std::endl << "Название трубы: " << Pipe.Name << "; Длина трубы: " << Pipe.length << "; Диаметр трубы: " << Pipe.diameter << "; Статус 'в ремонте': Да" << std::endl;
+    }
+    else {
+        std::cout << "Труба создана: " << std::endl << "Название трубы: " << Pipe.Name << "; Длина трубы: " << Pipe.length << "; Диаметр трубы: " << Pipe.diameter << "; Статус 'в ремонте': Нет" << std::endl;
+    };
+   
+}
+int main(){
+    setlocale(LC_ALL, "RU");
+    while (true) {
+        menu();
+        if (k > 10 || k < 0) {
+            std::cout << "ВВЕДИТЕ КОРРЕКТНУЮ КОМАНДУ!"<<std::endl;
+        }
+        if (k == 0) {
+            break;
+        }
+        if (k == 1) {
+            menu_new_pipe();
+        }
+    }
     return 0;
 }
